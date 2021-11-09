ee9707438b7f8ec8753e8e6841c082e719a36944
andrewrubinstein <andrew.rubinstein93@gmail.com>
2021-11-08 20:06:34 -0500

    Bug fix refresh sprite image where needed manually because copySprite no longer does automatically

--- web/spriteDraw.ts
+++ web/spriteDraw.ts
@@ -2,7 +2,7 @@ function sleep(ms):Promise<void> {
     return new Promise<void>(resolve => setTimeout(resolve, ms));
 }
 
-const dim = [128,128];
+const dim = [528,528];
 function threeByThreeMat(a:number[], b:number[]):number[]
 {
     return [a[0]*b[0]+a[1]*b[3]+a[2]*b[6], 
